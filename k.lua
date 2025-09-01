68c2a5970e8935d2e9c289511e27470e90c074c4
tnorfxpremium-max <tnorfxpremium@gmail.com>
2025-09-01 20:11:12 +0700

    Update k.lua

--- k.lua
+++ k.lua
@@ -1,10 +1,10 @@
 return {
     tnorf = {
         status = "active",
-        hwid   = "fcf5849f0499406d"
+        hwid   = { "57aad40619d021fa", "A1B2C3D4E5F6G7H8" }
     },
     testkey = {
         status = "banned",
-        hwid   = "FFFF-FFFF-FFFF-FFFF"
+        hwid   = { "FFFF-FFFF-FFFF-FFFF" }
     }
 }
