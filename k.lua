dea40a37ce65bf83a789a03b93f0e6c3721d5e78
tnorfxpremium-max <tnorfxpremium@gmail.com>
2025-09-01 18:47:02 +0700

    Update and rename k.json to k.lua

---
+++ k.lua
@@ -0,0 +1,10 @@
+return {
+    tnorf = {
+        status = "active",
+        hwid   = "57aad40619d021fa"
+    },
+    testkey = {
+        status = "banned",
+        hwid   = "FFFF-FFFF-FFFF-FFFF"
+    }
+}
